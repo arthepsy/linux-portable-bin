deb8a6ece922292e45c104cd6a958a7aa6382f92
Andris Raugulis <moo@arthepsy.eu>
2020-05-28 13:49:57 +0000

    Fix TravisCI errors (too long log file, keep empty directories). Stop build on first error.

---
+++ ci.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+_cdir=$(cd -- "$(dirname "$0")" && pwd)
+
+_fail() {
+	if [ "$1" -ne 0 ]; then
+		if [ -e "build.log" ]; then
+			cat "build.log"
+		fi
+		exit 1
+	fi
+}
+
+cd "${_cdir}/masscan" || exit 1
+for _arch in x86 x64; do
+	./run.sh build "${_arch}" head > build.log; _fail $?
+	./run.sh pack  "${_arch}" head; _fail $?
+	./run.sh build "${_arch}" > build.log; _fail $?
+	./run.sh pack "${_arch}"; _fail $?
+done
+cd "${_cdir}/socat" || exit 1
+for _arch in x86 x64; do
+	./run.sh build "${_arch}" > build.log; _fail $?
+	./run.sh pack  "${_arch}"; _fail $?
+	./run.sh build "${_arch}" "" weak-ssl > build.log; _fail $?
+	./run.sh pack  "${_arch}" "" weak-ssl; _fail $?
+done
+cd "${_cdir}/nmap" || exit 1
+for _arch in x86 x64; do
+	./run.sh build "${_arch}" head bad-ssl > build.log; _fail $?
+	./run.sh pack  "${_arch}" head bad-ssl; _fail $?
+	./run.sh build "${_arch}" head weak-ssl > build.log; _fail $?
+	./run.sh pack  "${_arch}" head weak-ssl; _fail $?
+	./run.sh build "${_arch}" "" bad-ssl > build.log; _fail $?
+	./run.sh pack  "${_arch}" "" bad-ssl; _fail $?
+	./run.sh build "${_arch}" "" weak-ssl > build.log; _fail $?
+	./run.sh pack  "${_arch}" "" weak-ssl; _fail $?
+done
+cd "${_cdir}/openssl" || exit 1
+	./run.sh build "${_arch}" "1.0.2-bad" zlib; _fail $?
+	./run.sh pack  "${_arch}" "1.0.2-bad" zlib; _fail $?
+	./run.sh build "${_arch}" "" zlib weak-ssl; _fail $?
+	./run.sh pack  "${_arch}" "" zlib weak-ssl; _fail $?
+cd "${_cdir}" || exit 1
+
