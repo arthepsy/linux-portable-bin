d0372197816296f9aedd139ee6b61f06aaa752c7
Andris Raugulis <moo@arthepsy.eu>
2024-01-11 21:59:52 +0000

    strip busybox. fix typos.

--- busybox/run.sh
+++ busybox/run.sh
@@ -15,9 +15,8 @@ _usage() {
 	printf "\tx86\tx86 32-bit (musl)\n"
 	printf "\tx64\tx86 64-bit (musl)\n"
 	printf "\nbusybox_version:\n"
-	printf "\thead\tlatest development branch\n"
-	printf "\tA.BC\tuse specific version, e.g., %s\n" "${_latest}"
-	printf "\nexample:\n      %s build x86 %s\n\n" "$0" "${_latest}"
+	printf "\tA.B.C\tuse specific version, e.g., %s\n" "${_latest}"
+	printf "\nexample:\n      %s build x64 %s\n\n" "$0" "${_latest}"
 	exit 1
 }
 
@@ -53,16 +52,18 @@ case $_action in
 			"build-busybox.sh" "${_ver}" "${_opt}" "${_debug}"
 		;;
 	pack)
-		! _check && _err "not built jet."
+		! _check && _err "not built yet."
 		cd -- "${_cdir}" || _err "cannot cd to ${_cdir}"
 		_fname="busybox-${_name}.${_target}"
 		_tmp="${_cdir}/out/tmp"; rm -rf -- "${_tmp}"; mkdir -p -- "${_tmp}" || _err "mkdir ${_tmp}"
-		echo "- dump ${_name}.${_target}"
+		echo "- dump ${_fname}"
 		docker run --rm \
 			-i "moo/static-busybox:${_name}.${_target}" \
 			tar -czf - -C /cross . > "${_tmp}/dump.tar.gz" || _err "docker+tar"
 		tar -xzf "${_tmp}/dump.tar.gz" -C "${_tmp}" || _err "tar"
 		_out="${_cdir}/out/${_fname}"; rm -rf -- "${_out}"; mkdir -p -- "${_out}" || _err "mkdir ${_out}"
+		echo "- strip ${_fname}"
+		_docker_strip "${_tmp}" "busybox"
 		echo "- pack ${_fname}"
 		cp "${_tmp}/busybox" "${_out}" || _err "cp"
 		rm -f -- "${_out}.tar.gz"
